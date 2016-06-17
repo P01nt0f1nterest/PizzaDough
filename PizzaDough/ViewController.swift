cb4cc6123e88374b35e0e646032495774cc6a94c
bje009 <bje009@bucknell.edu>
2016-06-17 00:39:16 -0500

    Initial Commit

---
+++ PizzaDough/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  PizzaDough
+//
+//  Created by Brett Enright on 5/8/16.
+//  Copyright © 2016 Bert's Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    ///
+    /// Calculates the cost per square inch of pizza with the given inputs
+    ///
+    ///  - parameter priceOfPizza: The price for a given pizza(s)
+    ///  - parameter numberOfPizzas: Pizzas that must be purchased to get price of priceOfPizza
+    ///  - parameter sizeOfPizza: The size of each pizza to be purchased (in inches)
+    ///  - parameter quantityOfPurchasedPizzas: Total number of pizzas to be purchased
+    ///  
+    ///  - returns costPerSquareInch: How much each square inch of pizza will cost
+    
+    func fillerFinalValueCalc(priceOfPizza: Double, numberOfPizzas: Int, sizeOfPizza: Double, quantityOfPurchasedPizzas: Int) {
+        
+        // TODO - Calculate and return costPerSquareInch
+        
+    }
+
+    ///
+    /// Once user has entered necessary info, will calculate and display value of each pizza
+    ///
+    @IBAction func CalculateValues(sender: AnyObject) {
+        
+        // TODO - Calculate and display values
+        
+    }
+}
+
